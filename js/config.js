087a7a295f3051b4ed9ddf1ef6c1fad93a426107
marioaaziz <marioadel61@gmail.com>
2025-06-06 21:21:32 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_0HALf5rEC', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '35rb88el9p11jmbpnhtr4s70du', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3c73asqgmudab5uc720hgbcc0n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
